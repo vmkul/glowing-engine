95cc79b8af3ebbecb5fef4b706c7758e6df3621e
vmkul <55065764+vmkul@users.noreply.github.com>
2020-01-20 23:33:05 +0200

    Update async

--- async.js
+++ async.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+
 const wrapAsync = fn => (...args) => setTimeout(() => fn(...args),
   Math.floor(Math.random() * 5000));
 
@@ -9,6 +11,25 @@ const sleep = time => {
   });
   return promise;
 };
+
+const aFunc = par => {
+	return new Promise((resolve, reject) => {
+	  if (!par) reject('No argument given!');
+	  setTimeout(() => resolve(par ** 2), 3000);	
+	}).then(console.log);
+};
+
+const asyncRead = file => {
+	return new Promise((resolve, reject) => {
+	  fs.readFile(file, 'utf-8', (err, data) => resolve(data.length));	
+	});
+};
+
+(async () => {
+
+
+
+})();
 
 const Func = async name => {
   await sleep(3000);
@@ -20,7 +41,10 @@ asyncFunc();
 
 (async () => {
 
-  console.time('1');
+  console.log(aFunc(4));
+  console.log(aFunc(6));
+  console.log(await asyncRead('async.js'));
+	console.time('1');
   const data = await Func('async.js');
   console.log(data);
   console.timeEnd('1');
